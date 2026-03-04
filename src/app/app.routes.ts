9b1ac6c26c795ec3ba0021cdbfd77375986cf11c
vidhishaprabhu <vidhishaprabhu@gmail.com>
2026-03-04 10:08:29 +0530

    Added the routerlink,router-outlet to header navbar links

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,68 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/home/home';
+import { Admission } from './features/admission/admission';
+import { Notice } from './features/home/notice/notice';
+import { Events } from './features/events/events';
+import { Gallery } from './features/gallery/gallery';
+import { Teachers } from './features/teachers/teachers';
+import { Contact } from './pages/contact/contact';
+import { About } from './pages/about/about';
+import { PrivacyPolicy } from './pages/privacy-policy/privacy-policy';
+import { Disclaimer } from './pages/disclaimer/disclaimer';
+import { Dashboard } from './dashboard/dashboard';
+import { Login } from './login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'',
+    component:Home
+  },
+  {
+    path:'admission',
+    component:Admission
+  },
+  {
+    path:'notices',
+    component:Notice
+  },
+  {
+    path:'events',
+    component:Events
+  },
+  {
+    path:'gallery',
+    component:Gallery
+  },
+  {
+    path:'teachers',
+    component:Teachers
+  },
+  {
+    path:'contact-us',
+    component:Contact 
+  },
+  {
+    path:'about-us',
+    component:About
+  },
+  {
+    path:'privacy-policy',
+    component:PrivacyPolicy
+  },
+  {
+    path:'disclaimer',
+    component:Disclaimer
+  },
+  {
+    path:'dashboard',
+    component:Dashboard
+  },
+  {
+    path:'login',
+    component:Login
+  },
+  {
+    path:'**',
+    redirectTo:'/home'
+  }
+];
