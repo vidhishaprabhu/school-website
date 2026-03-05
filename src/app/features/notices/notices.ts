0f28a110e043e980f84c4787fb81c1508585e92e
vidhishaprabhu <vidhishaprabhu@gmail.com>
2026-03-05 18:10:56 +0530

    Done with display of notices data

--- src/app/features/notices/notices.ts
+++ src/app/features/notices/notices.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
+import { Api } from '../../services/api';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-notices',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './notices.html',
   styleUrl: './notices.scss'
 })
 export class Notices {
+  notices:any[]=[];
+  selectedNotice:any={
+    title:'',
+    description:'',
+    date:'',
+    category:''
+  }
+  constructor(private apiService:Api){}
+  ngOnInit(){
+    this.getNotices();
+  }
+  getNotices(){
+    this.apiService.getNotice().subscribe({
+      next:(res:any)=>{
+        if(res){
+          this.notices=res.notice;
+        }
+      },
+      error:(error)=>{
+        console.error(error);
+      }
+    })
+  }
+  showNotice(notice:any){
+    this.selectedNotice=notice;
 
+  }
 }
