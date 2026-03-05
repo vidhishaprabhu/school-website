9378dce552f9f549dc0d6b3cbbf59f0d59e2d327
vidhishaprabhu <vidhishaprabhu@gmail.com>
2026-03-05 20:45:28 +0530

    Done with display data related to events

--- src/app/features/events/events.ts
+++ src/app/features/events/events.ts
@@ -1,14 +1,42 @@
 import { Component } from '@angular/core';
+import { Api } from '../../services/api';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-events',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './events.html',
-  styleUrl: './events.scss'
+  styleUrl: './events.scss',
 })
 export class Events {
-  ngOnInit(){
-    window.scrollTo(0,0);
+  events: any[] = [];
+  selectedEvents: any = {
+    title: '',
+    description: '',
+    shortDescription: '',
+    date: '',
+    location: '',
+  };
+  constructor(private apiService: Api) {}
+  ngOnInit() {
+    window.scrollTo(0, 0);
+    this.getEvents();
   }
-
+  getEvents() {
+    this.apiService.getEvents().subscribe({
+      next: (res: any) => {
+        if (res) {
+          this.events = res.events;
+          console.log(this.events);
+        }
+      },
+      error: (error) => {
+        console.error(error);
+      },
+    });
+  }
+  onEventClicking(event:any){
+    this.selectedEvents=event;
+    console.log(this.selectedEvents);
+  }
 }
