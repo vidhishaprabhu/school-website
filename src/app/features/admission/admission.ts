8a6b949a46c32c20eb287dddc413f226376653fe
vidhishaprabhu <vidhishaprabhu@gmail.com>
2026-03-05 15:15:18 +0530

    Done with working of frontend of generating pdf of admission form

--- src/app/features/admission/admission.ts
+++ src/app/features/admission/admission.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
 
 @Component({
   selector: 'app-admission',
@@ -15,12 +17,12 @@ export class Admission {
         fullName:['',Validators.required],
         dob:['',Validators.required],
         gender:['',Validators.required],
-        age:['',Validators.required,Validators.min(3),Validators.max(18)],
+        age:['', [Validators.required, Validators.min(3), Validators.max(18)]],
         bloodGroup:[''],
         religion:['',Validators.required],
         casteCategory:['',Validators.required],
         nationality:['',Validators.required],
-        montherTongue:['',Validators.required],
+        motherTongue:['',Validators.required],
         aadharNumber:['',Validators.required]
       }),
       admissionDetails:this.fb.group({
@@ -64,13 +66,58 @@ export class Admission {
         studentPhoto:[null,Validators.required]
 
       }),
-      declaration:fb.group({
+      declaration:this.fb.group({
         agree:[false,Validators.requiredTrue],
         declarationDate:['',Validators.required]
       })
   
 
     })
+  }
+  copyAddress(event:Event){
+    const isChecked=(event.target as HTMLInputElement).checked;
+    const present=this.admissionForm.get('addressDetails.presentAddress');
+    const permanent=this.admissionForm.get('addressDetails.permanentAddress');
+    if(isChecked && present && permanent){
+      permanent?.setValue(present.value)
+      permanent.disable();
+
+    }
+    else{
+      permanent?.enable();
+    }
+  }
+  generatePDF():void{
+    // console.log(this.admissionForm.value);
+    const content=document.getElementById('pdf');
+    if(content){
+      content.style.display='block';
+      html2canvas(content).then((canvas)=>{
+        const imgData=canvas.toDataURL('image/png');
+        const pdf=new jsPDF('p','mm','a4');
+        const imgProps=pdf.getImageProperties(imgData);
+        const pdfWidth=pdf.internal.pageSize.getWidth();
+        const pdfHeight=(imgProps.height * pdfWidth) / imgProps.width;
+        pdf.addImage(imgData,'PNG',0,0,pdfWidth,pdfHeight);
+        pdf.save('admission-formss.pdf');
+
+       
+      })
+      content.style.display='none';
+    }
+    
+    
+  }
+  onImageSelect(event:Event){
+    const file=(event.target as HTMLInputElement).files?.[0];
+    if(file){
+      const reader=new FileReader();
+      reader.onload=()=>{
+        this.admissionForm.get('documents.studentPhoto')?.setValue(reader.result);
+      }
+      reader.readAsDataURL(file);
+    }
+   
   }
 
 
