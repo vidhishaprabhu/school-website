5fb8e467a1090c40eff779831397b60dd347e76b
vidhishaprabhu <vidhishaprabhu@gmail.com>
2026-03-13 13:27:16 +0530

    Donw with add angular material animation called MatSnackBarModule

--- src/app/register/register.ts
+++ src/app/register/register.ts
@@ -3,35 +3,55 @@ import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angula
 import { Auth } from '../services/auth';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-register',
-  imports: [ReactiveFormsModule,CommonModule],
+  imports: [ReactiveFormsModule,CommonModule,MatSnackBarModule],
+  standalone:true,
   templateUrl: './register.html',
   styleUrl: './register.scss'
 })
 export class Register {
   registerForm:FormGroup;
-  constructor(private authService:Auth,private fb:FormBuilder,private router:Router){
+  constructor(private authService:Auth,private fb:FormBuilder,private router:Router,private snackBar: MatSnackBar){
     this.registerForm=fb.group({
       name:['',Validators.required],
       email:['',[Validators.required,Validators.email]],
       password:['',[Validators.required,Validators.max(3)]]
     })
   }
-  register(){
-    // console.log(this.registerForm.value);
-    this.authService.register(this.registerForm.value.name,this.registerForm.value.email,this.registerForm.value.password).subscribe((res:any)=>{
-      if(res){
-        alert(res.message);
-        this.router.navigateByUrl('/login')
-        this.registerForm.reset();
+  register() {
+
+  this.authService.register(
+    this.registerForm.value.name,
+    this.registerForm.value.email,
+    this.registerForm.value.password
+  ).subscribe({
+
+    next: (res: any) => {
+      this.snackBar.open(res.message, "Close", {
+        duration: 3000
+      });
+      this.router.navigateByUrl('/login');
+      this.registerForm.reset();
+    },
+
+    error: (err) => {
+      if (err.error && err.error.message) {
+        this.snackBar.open(err.error.message, "Close", {
+        duration: 3000
+      });
+      } else {
+        this.snackBar.open(err.error.message, "Close", {
+        duration: 3000
+      });
       }
-      else{
-        console.error('There is some error');
-      }
-    })
-  }
+    }
+
+  });
+
+}
 
 
 
