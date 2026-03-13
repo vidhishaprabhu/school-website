0a66a3fd2ad4ca2f7586e95d08735b98e3b09c03
vidhishaprabhu <vidhishaprabhu@gmail.com>
2026-03-13 13:59:42 +0530

    Added the MatSnackBar to login form

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,43 +1,64 @@
 import { CommonModule, JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { Auth } from '../services/auth';
 import { Router } from '@angular/router';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login',
-  imports: [ReactiveFormsModule,CommonModule],
+  imports: [ReactiveFormsModule, CommonModule, MatSnackBarModule],
   templateUrl: './login.html',
-  styleUrl: './login.scss'
+  styleUrl: './login.scss',
 })
 export class Login {
-  loginForm:FormGroup;
-  constructor(private formBuilder:FormBuilder,private authService:Auth,private router:Router){
-    this.loginForm=this.formBuilder.group({
-      email:['',[Validators.required,Validators.email]],
-      password:['',Validators.required]
-    })
-
-  }
-  onLogin(){
-    console.log(this.loginForm.value);
-    this.authService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe({
-      next: (res: any) => {
-        if (res) {
-          const token=res.token;
-          if(token){
-            localStorage.setItem('token',token);
-            localStorage.setItem('user',JSON.stringify(res.user))
-            this.authService.startSessionTimer();
-            this.router.navigate(['/dashboard'])
-          }
-        }
-      },
-      error: (error) => {
-        console.error(error);
-      },
+  loginForm: FormGroup;
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: Auth,
+    private router: Router,
+    private snackBar: MatSnackBar,
+  ) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
     });
   }
-
-
+  onLogin() {
+    console.log(this.loginForm.value);
+    this.authService
+      .login(this.loginForm.value.email, this.loginForm.value.password)
+      .subscribe({
+        next: (res: any) => {
+          if (res) {
+            const token = res.token;
+            if (token) {
+              localStorage.setItem('token', token);
+              localStorage.setItem('user', JSON.stringify(res.user));
+              this.authService.startSessionTimer();
+              this.router.navigate(['/dashboard']);
+            }
+          }
+          this.snackBar.open(res.message, 'Close', {
+            duration: 3000,
+          });
+        },
+        error: (err) => {
+          if (err.error && err.error.message) {
+            this.snackBar.open(err.error.message, 'Close', {
+              duration: 3000,
+            });
+          } else {
+            this.snackBar.open(err.error.message, 'Close', {
+              duration: 3000,
+            });
+          }
+        },
+      });
+  }
 }
--- src/app/register/register.ts
+++ src/app/register/register.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { Auth } from '../services/auth';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
@@ -7,53 +12,52 @@ import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-register',
-  imports: [ReactiveFormsModule,CommonModule,MatSnackBarModule],
-  standalone:true,
+  imports: [ReactiveFormsModule, CommonModule, MatSnackBarModule],
+  standalone: true,
   templateUrl: './register.html',
-  styleUrl: './register.scss'
+  styleUrl: './register.scss',
 })
 export class Register {
-  registerForm:FormGroup;
-  constructor(private authService:Auth,private fb:FormBuilder,private router:Router,private snackBar: MatSnackBar){
-    this.registerForm=fb.group({
-      name:['',Validators.required],
-      email:['',[Validators.required,Validators.email]],
-      password:['',[Validators.required,Validators.max(3)]]
-    })
+  registerForm: FormGroup;
+  constructor(
+    private authService: Auth,
+    private fb: FormBuilder,
+    private router: Router,
+    private snackBar: MatSnackBar,
+  ) {
+    this.registerForm = fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.max(3)]],
+    });
   }
   register() {
+    this.authService
+      .register(
+        this.registerForm.value.name,
+        this.registerForm.value.email,
+        this.registerForm.value.password,
+      )
+      .subscribe({
+        next: (res: any) => {
+          this.snackBar.open(res.message, 'Close', {
+            duration: 3000,
+          });
+          this.router.navigateByUrl('/login');
+          this.registerForm.reset();
+        },
 
-  this.authService.register(
-    this.registerForm.value.name,
-    this.registerForm.value.email,
-    this.registerForm.value.password
-  ).subscribe({
-
-    next: (res: any) => {
-      this.snackBar.open(res.message, "Close", {
-        duration: 3000
+        error: (err) => {
+          if (err.error && err.error.message) {
+            this.snackBar.open(err.error.message, 'Close', {
+              duration: 3000,
+            });
+          } else {
+            this.snackBar.open(err.error.message, 'Close', {
+              duration: 3000,
+            });
+          }
+        },
       });
-      this.router.navigateByUrl('/login');
-      this.registerForm.reset();
-    },
-
-    error: (err) => {
-      if (err.error && err.error.message) {
-        this.snackBar.open(err.error.message, "Close", {
-        duration: 3000
-      });
-      } else {
-        this.snackBar.open(err.error.message, "Close", {
-        duration: 3000
-      });
-      }
-    }
-
-  });
-
-}
-
-
-
-
+  }
 }
